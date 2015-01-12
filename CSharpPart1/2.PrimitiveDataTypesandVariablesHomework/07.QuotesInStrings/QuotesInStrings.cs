d16dc2f2fee2abaf787a506d8ca3e67a6147a1dd
Petar-Milchev <petarstz90@gmail.com>
2015-01-12 19:31:00 +0200

    2.Primitive Data Types and Variables Homework
    Second Homerwork

---
+++ CSharpPart1/2.PrimitiveDataTypesandVariablesHomework/07.QuotesInStrings/QuotesInStrings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+    class QuotesInStrings
+    {
+        static void Main(string[] args)
+        {
+            string first = "The \"use\" of quotations causes difficulties.";
+            string second = @"The ""use"" of quotations causes difficulties.";
+            Console.WriteLine("{0}\n{1}" , first, second);
+        }
+    }
+
---
+++ CSharpPart1/2.PrimitiveDataTypesandVariablesHomework/DeclareVariables.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class DeclareVariables
+{
+    static void Main(string[] args)
+    {
+        ushort numberOne = 52130;
+        sbyte numberTwo = -115;
+        int numberThree = 4825932;
+        byte numberFour = 97;
+        short numberFive = -10000;
+        Console.WriteLine(numberOne + "\n" + numberTwo + "\n" + numberThree + "\n" + numberFour + "\n" + numberFive);
+    }
+}
+
